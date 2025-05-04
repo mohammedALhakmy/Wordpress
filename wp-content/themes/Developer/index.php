8a4bc14278af30f96e8b812ea69203c29afcf673
mohammedALhakmy <mohammed.devlopement.yemen@gmail.com>
2025-05-04 17:34:20 +0300

    add func the add css

--- wp-content/themes/Developer/index.php
+++ wp-content/themes/Developer/index.php
@@ -1,9 +1,7 @@
 <?php get_header();?>
 
 This Theme Developer Mohammed
-<?php get_sidebar();?>
 
-<?php get_search_form();?>
-
+<?php echo get_template_directory_uri() . '/css/bootstrap.min.css'; ?>
 
 <?php get_footer();
