5ae2a395f4c8a034a404b26b7a09cf33384e4ec2
mohammedALhakmy <mohammed.devlopement.yemen@gmail.com>
2025-05-05 16:00:40 +0300

    some change and add files

--- wp-content/themes/Developer/functions.php
+++ wp-content/themes/Developer/functions.php
@@ -3,5 +3,16 @@
 
 function developer_add_styles()
 {
+    wp_enqueue_style('my-bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css');
+    wp_enqueue_style('font-awesome', get_template_directory_uri() . '/css/font-awesome.min.css');
+}
+
+function developer_add_scripts()
+{
+    wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), false, true);
+    wp_enqueue_script('main-js', get_template_directory_uri() . '/js/main.js', array('jquery'), false, true);
 
 }
+
+add_action('wp_enqueue_scripts', 'developer_add_scripts');
+add_action('wp_enqueue_scripts', 'developer_add_styles');
