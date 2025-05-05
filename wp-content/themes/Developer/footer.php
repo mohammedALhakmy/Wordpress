2f56c1442b62e090a353a2ab093fb795b319c9d1
mohammedALhakmy <mohammed.devlopement.yemen@gmail.com>
2025-05-05 17:17:49 +0300

    some change and add files12

--- wp-content/themes/Developer/footer.php
+++ wp-content/themes/Developer/footer.php
@@ -1,7 +1,7 @@
 
 
 
-<div class="footer text-center bg-info">Copyright © 2020 - <?php echo date('Y') . ' ' . get_bloginfo('name'); ?></div>
+<div class="footer text-center">Copyright © 2020 - <?php echo date('Y') . ' ' . get_bloginfo('name'); ?></div>
 
 <?php wp_footer();?>
 </body>
