bf9c0f1f1073401e3c10aa3e4b76aaca391d91e8
mohammedALhakmy <mohammed.devlopement.yemen@gmail.com>
2025-05-06 17:21:31 +0300

    add function nav menu hader bootstarp nav2

--- wp-content/themes/Developer/functions.php
+++ wp-content/themes/Developer/functions.php
@@ -30,7 +30,10 @@ function developer_register_custom_menu(){
 add_action('init', 'developer_register_custom_menu');
 
 function developer_bootstrap_menu(){
-    wp_nav_menu();
+    wp_nav_menu([
+        'theme-location' => 'bootstrap-menu',
+        'menu-class' => 'mohammed'
+    ]);
 }
 {
 
