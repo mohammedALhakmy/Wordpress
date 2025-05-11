750de3575ba751b1d2f711c81780b5c8d26cea04
mohammedALhakmy <mohammed.devlopement.yemen@gmail.com>
2025-05-11 16:30:03 +0300

    add Sction page

--- wp-content/themes/Developer/index.php
+++ wp-content/themes/Developer/index.php
@@ -1,7 +1,22 @@
 <?php echo get_header();?>
 
-This Theme Developer Mohammed
-
-<?php echo get_template_directory_uri() . '/css/bootstrap.min.css'; ?>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="main-post">
+                <h3 class="post-title">This is Our Main Post Title</h3>
+                <span class="post-author">Mohammed</span>
+                <span class="post-date">20/05/2025</span>
+                <span class="post-comments">20 Comments</span>
+                <img src="https://placehold.it/300x100/300" alt="">
+                <p class="post-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                <hr>
+                <p class="categories">
+                    HTML,Css,JS,Vue,PHP
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php echo get_footer();
