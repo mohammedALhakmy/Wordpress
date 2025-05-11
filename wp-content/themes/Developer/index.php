8e9417b50e6b2550e070f635307aa95afe425770
mohammedALhakmy <mohammed.devlopement.yemen@gmail.com>
2025-05-11 16:57:12 +0300

    add Sction page5

--- wp-content/themes/Developer/index.php
+++ wp-content/themes/Developer/index.php
@@ -5,13 +5,14 @@
         <div class="col-sm-6">
             <div class="main-post">
                 <h3 class="post-title">This is Our Main Post Title</h3>
-                <span class="post-author">Mohammed</span>
-                <span class="post-date">20/05/2025</span>
-                <span class="post-comments">20 Comments</span>
+                <span class="post-author"><i class="fa fa-user fa-fw fa-lg"></i>Mohammed, </span>
+                <span class="post-date"><i class="fa fa-calendar fa-fw fa-lg"></i>20/05/2025, </span>
+                <span class="post-comments"><i class="fa fa-comments-o fa-fw fa-lg"></i>20 Comments</span>
                 <img class="img-responsive img-thumbnail" width="600px" height="200px" src="https://www.w3schools.com/html/pic_trulli.jpg" alt="Trulli">
                 <p class="post-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                 <hr>
                 <p class="categories">
+                    <i class="fa fa-tags fa-fw fa-lg"></i>
                     HTML,Css,JS,Vue,PHP
                 </p>
             </div>
